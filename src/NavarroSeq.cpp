fbfa0e07b440d226c9053bd3c4e13030a4c7b203
Matt Jenny <mjenny@d-172-25-178-24.dhcp.virginia.edu>
2014-02-23 17:26:50 -0500

    Added helper methods to enumerate all possible combinations R.

--- src/NavarroSeq.cpp
+++ src/NavarroSeq.cpp
@@ -1,10 +1,39 @@
 #include "NavarroSeq.h"
 #include <string>
+#include <math.h>
+#include <unordered_set>
+#include <stdio.h>
+#include <vector>
 
 using namespace std;
 
 string NavarroSeq::compress(string s)
 {
+	size_t n = s.size();
+	size_t i,r;
+
+	unordered_set<char> unique_chars;
+	for (i=0; i<n; i++) {
+		unique_chars.insert(s.at(i));
+	}
+	r = unique_chars.size();
+
+	size_t u = floor(0.5*log((double) n)/log((double) r));
+	printf("n = %i; r = %i; u = %i\n", (int)n, (int)r, (int)u);
+
+	//example enumeration (while it's still fresh on my mind) :
+
+	vector<vector<unsigned int> > combinations = get_all_combinations(unique_chars.size(), u);
+	printf ("Vector size is %u\n", (unsigned int) combinations.size());
+
+	// get a chunk of u characters
+	// make a vector of length r containing numbers of each character
+
+	// I
+	// R
+	// E tables
+	// F mapping
+
 	return "Hello World!";
 }
 
@@ -28,6 +57,45 @@ unsigned int NavarroSeq::select(string s, char c, int index)
 	return 55;
 }
 
+vector<vector<unsigned int> > NavarroSeq::get_all_combinations(size_t r, size_t u) // r is alphabet size; u is block size
+{
+	unsigned int i;
+	vector<vector<unsigned int> > combinations; // store a list of all combinations
+
+	for (i = 0; i<r; i++)
+	{
+		/*
+		* Create a vector of counts for each character
+		* Vector of length r
+		* values[0] has counts for alphabet[0], values[1] for alphabet[1]...
+		*/
+		vector<unsigned int> values (r,0);
+		values.at(i)++; // Set initial count to current character
+		enumerate(values, combinations, i, r, u-1); // add all enumerations with this prefix
+	}
+	return combinations;
+}
+
+void NavarroSeq::enumerate(vector<unsigned int>& values, vector<vector<unsigned int> >& combos, unsigned int min, unsigned int r, unsigned int u)
+{
+	if (u <= 0) //We already have a sequence of length u
+	{
+		combos.push_back(values); // Add it to the list
+		return;
+	}
+	else
+	{
+		unsigned int i;
+		for (i = min; i<r; i++) // iterate through all possible characters >= prefix
+		{
+			values.at(i)++;
+			enumerate(values, combos, i, r, u-1);
+		}
+	}
+}
+
 int main() {
+	// test sequence
+	NavarroSeq::compress("abbabaababbababcabbabaababbababcabbabaababbababcabbabaababbababcabbabaababbababcabc");
 	return 0;
 }
--- src/NavarroSeq.h
+++ src/NavarroSeq.h
@@ -2,16 +2,23 @@
 # define NAVARROSEQ_H
 
 #include <string>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class NavarroSeq
 {
+public:
 	static string compress(string s);
 	static string decompress(string s);
 	static char access(string s, int index);
 	static unsigned int rank(string s, char c, int index);
 	static unsigned int select(string s, char c, int index);
+
+private:
+	static vector<vector<unsigned int> > get_all_combinations(size_t r, size_t u);
+	static void enumerate(vector<unsigned int>& values, vector<vector<unsigned int> >& combos, unsigned int min, unsigned int r, unsigned int u);
 };
 
 # endif
